68d88ac0a17464a1ccc5cde99d4879c19667acc9
Andrew Davis <andrew@andrewrdavis.net>
2013-03-03 21:15:29 -0500

    starting quiz 1 attempt, lots to do still

--- 1_solitaire.rb
+++ 1_solitaire.rb
@@ -1,10 +1,91 @@
-# Code in Ruby, live longer!
-# CODEI NRUBY LIVEL ONGER
-# DWJXH YRFDG TMSHP UURXJ
-# 3 15 4 5 9  14 18 21 2 25  12 9 22 5 12  15 14 7 5 18
-# 4 23 10 24 8  25 18 6 4 7  20 13 19 8 16  21 21 18 24 10
-# 7 12 14 3 17  13 10 1 6 6  6 22 15 13 2  10 9 25 3 2
-# GLNCQ MJAFF FVOMB JIYCB
+class Solitaire
 
-phrase = 'Code in Ruby, live longer!'
-puts phrase
+  attr_accessor :phrase, :keystream, :message1, :message2, :sum_message, :encryption
+
+  def initialize(phrase: 'Passphrase')
+    @phrase = phrase
+  end
+
+  def to_s
+    @phrase
+  end
+
+  def encrypt
+    simplify
+    generate_keystream
+    @message1 = @phrase.to_numeric_message
+    @message2 = @keystream.to_numeric_message
+    @sum_message = [@message1, @message2].transpose.map{|x| x.reduce :+}.under_26_only
+    @encryption = @sum_message.to_alpha_message
+  end
+
+  # Discard any non A to Z characters, and uppercase all remaining characters.
+  # Split the message into five character groups, using Xs to pad the last group, if needed.
+  def simplify
+    @phrase.upcase!
+    @phrase.remove_non_word_characters!
+    @phrase.append_xs!
+    @phrase.split_into_groups_of_five!
+  end
+
+  def generate_keystream
+    @keystream = 'CLEPK HHNIY CFPWH FDFEH'
+  end
+
+end
+
+class Array
+
+  def under_26_only
+    array = []
+    self.each do |num|
+      number = num
+      number = num - 26 if number > 26
+      array << number
+    end
+    array
+  end
+
+  def to_alpha_message
+    string = ''
+    self.each_with_index do |num, i|
+      string << number_to_letter(num)
+      string << ' ' if (i+1) % 5 == 0
+    end
+    string.strip
+  end
+
+  def number_to_letter(n)
+    n.to_s(27).tr("0-9a-q", "A-Z")
+  end
+end
+
+class String
+
+  def remove_non_word_characters!
+    gsub!(/\W/, '')
+  end
+
+  def append_xs!
+    self << 'X' until size % 5 == 0
+  end
+
+  def split_into_groups_of_five!
+    replace scan(/.{5}/).to_a.join(' ')
+  end
+
+  def to_numeric_message
+    array = []
+    self.split('').each do |char|
+      array << ('A'..'Z').to_a.index(char)
+    end
+    array.compact
+  end
+
+end
+
+
+# Execution
+solitaire = Solitaire.new(phrase: 'Code in Ruby, live longer!')
+solitaire.encrypt
+puts solitaire.encryption.inspect
