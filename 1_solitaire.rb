3b8c450b2d3a02cf8ccf084810352d1451ddcc73
Andrew Davis <andrew@andrewrdavis.net>
2013-03-03 20:00:49 -0500

    Initial commit

---
+++ 1_solitaire.rb
@@ -0,0 +1,10 @@
+# Code in Ruby, live longer!
+# CODEI NRUBY LIVEL ONGER
+# DWJXH YRFDG TMSHP UURXJ
+# 3 15 4 5 9  14 18 21 2 25  12 9 22 5 12  15 14 7 5 18
+# 4 23 10 24 8  25 18 6 4 7  20 13 19 8 16  21 21 18 24 10
+# 7 12 14 3 17  13 10 1 6 6  6 22 15 13 2  10 9 25 3 2
+# GLNCQ MJAFF FVOMB JIYCB
+
+phrase = 'Code in Ruby, live longer!'
+puts phrase
